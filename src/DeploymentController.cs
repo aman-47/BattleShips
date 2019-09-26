7bea2d0bc6253bc3f887d3239b9f0e372817bfc6
Harry Swain <harry.j.swain@gmail.com>
2019-09-26 15:22:03 +1000

    fixed errors in EndingGameController.cs

--- src/DeploymentController.cs
+++ src/DeploymentController.cs
@@ -75,14 +75,14 @@ namespace BattleShips
                 else
                     DoDeployClick ();
 
-                if (GameController.HumanPlayer.ReadyToDeploy & IsMouseInRectangle [PLAY_BUTTON_LEFT, TOP_BUTTONS_TOP, PLAY_BUTTON_WIDTH, TOP_BUTTONS_HEIGHT])
-                    EndDeployment ();
-                else if (IsMouseInRectangle [UP_DOWN_BUTTON_LEFT, TOP_BUTTONS_TOP, DIR_BUTTONS_WIDTH, TOP_BUTTONS_HEIGHT])
+                if (GameController.HumanPlayer.ReadyToDeploy & UtilityFunctions.IsMouseInRectangle (PLAY_BUTTON_LEFT, TOP_BUTTONS_TOP, PLAY_BUTTON_WIDTH, TOP_BUTTONS_HEIGHT))
+                    GameController.EndDeployment ();
+                else if (UtilityFunctions.IsMouseInRectangle(UP_DOWN_BUTTON_LEFT, TOP_BUTTONS_TOP, DIR_BUTTONS_WIDTH, TOP_BUTTONS_HEIGHT))
                     _currentDirection = Direction.LeftRight;
-                else if (IsMouseInRectangle [LEFT_RIGHT_BUTTON_LEFT, TOP_BUTTONS_TOP, DIR_BUTTONS_WIDTH, TOP_BUTTONS_HEIGHT])
+                else if (UtilityFunctions.IsMouseInRectangle (LEFT_RIGHT_BUTTON_LEFT, TOP_BUTTONS_TOP, DIR_BUTTONS_WIDTH, TOP_BUTTONS_HEIGHT))
                     _currentDirection = Direction.LeftRight;
-                else if (IsMouseInRectangle [RANDOM_BUTTON_LEFT, TOP_BUTTONS_TOP, RANDOM_BUTTON_WIDTH, TOP_BUTTONS_HEIGHT])
-                    HumanPlayer.RandomizeDeployment ();
+                else if (UtilityFunctions.IsMouseInRectangle (RANDOM_BUTTON_LEFT, TOP_BUTTONS_TOP, RANDOM_BUTTON_WIDTH, TOP_BUTTONS_HEIGHT))
+                    GameController.HumanPlayer.RandomizeDeployment ();
             }
         }
 
@@ -102,17 +102,17 @@ namespace BattleShips
 
             // Calculate the row/col clicked
             int row, col;
-            row = Convert.ToInt32 (Math.Floor ((mouse.Y) / (CELL_HEIGHT + CELL_GAP)));
-            col = Convert.ToInt32 (Math.Floor ((mouse.X - FIELD_LEFT) / (CELL_WIDTH + CELL_GAP)));
+            row = Convert.ToInt32 (Math.Floor ((mouse.Y) / (UtilityFunctions.CELL_HEIGHT + UtilityFunctions.CELL_GAP)));
+            col = Convert.ToInt32 (Math.Floor ((mouse.X - UtilityFunctions.FIELD_LEFT) / (UtilityFunctions.CELL_WIDTH + UtilityFunctions.CELL_GAP)));
 
-            if ((row >= 0) & (row < HumanPlayer.PlayerGrid.Height)) {
-                if ((col >= 0) & (col < HumanPlayer.PlayerGrid.Width)) {
+            if ((row >= 0) & (row < GameController.HumanPlayer.PlayerGrid.Height)) {
+                if ((col >= 0) & (col < GameController.HumanPlayer.PlayerGrid.Width)) {
                     // if in the area try to deploy
                     try {
-                        HumanPlayer.PlayerGrid.MoveShip (row, col, _selectedShip, _currentDirection);
+                        GameController.HumanPlayer.PlayerGrid.MoveShip (row, col, _selectedShip, _currentDirection);
                     } catch (Exception ex) {
-                        Audio.PlaySoundEffect (GameSound ["Error"]);
-                        Message = ex.Message;
+                        Audio.PlaySoundEffect (GameResources.GameSound ("Error"));
+                        UtilityFunctions.Message = ex.Message;
                     }
                 }
             }
@@ -124,21 +124,21 @@ namespace BattleShips
         /// </summary>
         public static void DrawDeployment ()
         {
-            DrawField [HumanPlayer.PlayerGrid, HumanPlayer, true];
+            UtilityFunctions.DrawField (GameController.HumanPlayer.PlayerGrid, GameController.HumanPlayer, true);
 
             // Draw the Left/Right and Up/Down buttons
             if (_currentDirection == Direction.LeftRight)
-                SwinGame.DrawBitmap (GameImage ["LeftRightButton"], LEFT_RIGHT_BUTTON_LEFT, TOP_BUTTONS_TOP);
+                SwinGame.DrawBitmap (GameResources.GameImage ("LeftRightButton"), LEFT_RIGHT_BUTTON_LEFT, TOP_BUTTONS_TOP);
             else
-                SwinGame.DrawBitmap (GameImage ["UpDownButton"], LEFT_RIGHT_BUTTON_LEFT, TOP_BUTTONS_TOP);
+                SwinGame.DrawBitmap (GameResources.GameImage ("UpDownButton"), LEFT_RIGHT_BUTTON_LEFT, TOP_BUTTONS_TOP);
 
             // DrawShips
             foreach (ShipName sn in Enum.GetValues (typeof (ShipName))) {
-                int i = default (int);
-                i = Int [sn] - 1;
+                int i = 0;
+                i = (int)sn - 1;
                 if (i >= 0) {
                     if (sn == _selectedShip)
-                        SwinGame.DrawBitmap (GameImage ["SelectedShip"], SHIPS_LEFT, SHIPS_TOP + (i * SHIPS_HEIGHT));
+                        SwinGame.DrawBitmap (GameResources.GameImage ("SelectedShip"), SHIPS_LEFT, SHIPS_TOP + (i * SHIPS_HEIGHT));
                 }
             }
 
@@ -157,10 +157,10 @@ namespace BattleShips
         private static ShipName GetShipMouseIsOver ()
         {
             foreach (ShipName sn in Enum.GetValues (typeof (ShipName))) {
-                int i = default (int);
-                i = Int [sn] - 1;
+                int i = 0;
+                i = (int) sn - 1;
 
-                if (IsMouseInRectangle [SHIPS_LEFT, SHIPS_TOP + (i * SHIPS_HEIGHT), SHIPS_WIDTH, SHIPS_HEIGHT])
+                if (UtilityFunctions.IsMouseInRectangle (SHIPS_LEFT, SHIPS_TOP + (i * SHIPS_HEIGHT), SHIPS_WIDTH, SHIPS_HEIGHT))
                     return sn;
             }
 
